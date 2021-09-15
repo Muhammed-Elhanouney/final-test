62bada9ef2b0c05991fe3831f09e5bab10476a8c
Muhammed-Elhanouney <muhammedelhanouney@gmail.com>
2021-09-15 20:41:38 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// $(document).ready(function(){
+//     $(".service-carousel").owlCarousel({
+//         loop:false,
+//         margin:10,
+//         nav:true,
+//         responsive:{
+//             0:{
+//                 items:1
+//             },
+//             600:{
+//                 items:3
+//             },
+//             1000:{
+//                 items:1
+// rtl:true,
+//             }
+//         }
+//     });
+//   });
+
+  
+   
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        rtl:true,
+        items:1,
+        nav:true,
+        margin:30,
+  
+    });
+  });
+
