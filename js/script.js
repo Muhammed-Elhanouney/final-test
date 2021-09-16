1f6158ae489ec821b231b42a355638d1be8985da
Muhammed-Elhanouney <muhammedelhanouney@gmail.com>
2021-09-16 17:23:54 +0200

    new one

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
         loop: true,
         margin: 30,
         nav: true, 
+        autoplay:true,
+    autoplayTimeout:2000,
+    // autoplayHoverPause:true,
         responsive: {
             0: {
                 items: 1
@@ -23,6 +26,8 @@ $('.clients .owl-carousel').owlCarousel({
     loop: true,
     margin: 15,
     nav: true,
+    autoplay:true,
+    autoplayTimeout:2000,
     responsive: {
         0: {
             items: 1
